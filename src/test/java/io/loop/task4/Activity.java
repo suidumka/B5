f14e6e548ca1c3093a4744899cda60aa3f525c4c
zhalaabbaszada <zhalaabbaszada@gmail.com>
2025-09-30 01:04:05 -0400

    some changes after conflict

--- src/test/java/io/loop/task4/Activity.java
+++ src/test/java/io/loop/task4/Activity.java
@@ -1,11 +1,7 @@
 package io.loop.task4;
 
 public class Activity extends SideMenuElement {
-    public static void  click (String button){
-        System.out.println("Duble click" + button);
-    }
     public static void click (String button){
-
 
         System.out.println("Double click " + button);
 
@@ -18,9 +14,7 @@ public class Activity extends SideMenuElement {
 
     public static void main(String[] args) {
         click("Activity");
-
         click("activity button");
-        click("activity");
     }
 
 }
--- src/test/java/io/loop/task4/SideMenuElement.java
+++ src/test/java/io/loop/task4/SideMenuElement.java
@@ -4,16 +4,9 @@ public class SideMenuElement {
 
     public static String color = "Grey";
     public String name;
+
     public static void click (String button){
         System.out.println("Click on " + button);
-
-    public void loadPage(String page) {
-        System.out.println("Loading page = " + page);
-    }
-
-    public static void  click (String button){
-        System.out.println("Click on" + button);
-    }
     }
     public void loadPage(String page){
         System.out.println("Loading page " + page);
@@ -22,8 +15,6 @@ public class SideMenuElement {
     public static void main(String[] args) {
         System.out.println(color);
         click("SideMenuElement");
-        System.out.println("color = " + color);
-        click("side menu element");
     }
 }
 
